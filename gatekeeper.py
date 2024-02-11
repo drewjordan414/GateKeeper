636cd0d4daf8c8d2fb2ce7d57f84a6d6651f5f3c
Drew Jordan <ajordan12@luc.edu>
2024-02-10 19:07:02 -0600

    updates

--- gatekeeper.py
+++ gatekeeper.py
@@ -144,8 +144,9 @@ def main():
 
         elif choice == "3":
             # Delete account
-            name = input("Enter the name of the account: ")
-            passwords_collection.delete_one({"name": name})
+            name = input("Enter the name of the account: ").lower()
+            account = passwords_collection.find_one({"name": {"$regex": f"^{name}$", "$options": "i"}})  # Convert input to lowercase
+            accounts.delete_one({"name": name})
             print("Account deleted successfully!")
         elif choice == "4":
             # View all accounts
